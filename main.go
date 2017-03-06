e293062603d6ee000be9c60c07f98d62a111d786
uryyyyyyy <koki305@gmail.com>
2017-03-06 20:29:50 +0900

    server

--- main.go
+++ main.go
@@ -2,14 +2,44 @@ package main
 
 import (
 	"fmt"
-	"github.com/uryyyyyyy/goSample/calc"
-	"github.com/golang/glog"
+	"net/http"
+	"log"
+	"os"
+	"io"
+	"strconv"
 )
 
+const port int = 9090
+const logFile string = "./application.log"
+
+func sayhelloName(w http.ResponseWriter, r *http.Request) {
+	log.Println("path", r.URL.Path)
+	log.Println("scheme", r.URL.Scheme)
+	fmt.Fprintf(w, "Hello world!")
+}
+
+func server() {
+	http.HandleFunc("/", sayhelloName) //アクセスのルーティングを設定します。
+	log.Println("start server at port:", strconv.Itoa(port))
+	err := http.ListenAndServe(":" + strconv.Itoa(port), nil) //監視するポートを設定します。
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
+
 func main() {
-	var str = "world"
-	fmt.Printf("hello, %s\n", str)
-	var a int = calc.Sum(2, 7)
-	glog.Info("Prepare to repel boarders")
-	fmt.Printf("sum: %d\n", a)
+	logfile, err := os.OpenFile(logFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		panic("cannnot open " + logFile + ":" + err.Error())
+	}
+	defer logfile.Close()
+	// io.MultiWriteで、
+	// 標準出力とファイルの両方を束ねて、
+	// logの出力先に設定する
+	log.SetOutput(io.MultiWriter(logfile, os.Stdout))
+
+	log.SetFlags(log.Ldate | log.Ltime)
+	log.Println("Log!!")
+
+	server()
 }
