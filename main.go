5ffb84a472cc418aa62e122c7deff9e3c22b00b6
uryyyyyyy <koki305@gmail.com>
2017-03-07 10:00:53 +0900

    json & encode

--- main.go
+++ main.go
@@ -2,14 +2,62 @@ package main
 
 import (
 	"fmt"
-	"github.com/uryyyyyyy/goSample/calc"
-	"github.com/golang/glog"
+	"encoding/json"
+	"net/url"
 )
 
+const jsonStr string = `
+{
+  "name": "日本",
+  "prefectures": [
+    {
+      "name": "東京都",
+      "capital": "東京",
+      "population": 13482040
+    },
+    {
+      "name": "埼玉県",
+      "capital": "さいたま市",
+      "population": 7249287
+    },
+    {
+      "name": "神奈川県",
+      "capital": "横浜市",
+      "population": 9116252
+    }
+  ]
+}
+`
+
+type Country struct {
+	Name string `json:"name"`
+	Prefectures []Prefecture `json:"prefectures"`
+}
+
+type Prefecture struct {
+	Name string    `json:"name"`
+}
+
 func main() {
-	var str = "world"
-	fmt.Printf("hello, %s\n", str)
-	var a int = calc.Sum(2, 7)
-	glog.Info("Prepare to repel boarders")
-	fmt.Printf("sum: %d\n", a)
+	var jsonBytes []byte = ([]byte)(jsonStr)
+	var data Country
+
+	if err := json.Unmarshal(jsonBytes, &data); err != nil {
+		fmt.Println("JSON Unmarshal error:", err)
+		return
+	}
+
+	v := url.Values{}
+	v.Set("name", "Ava")
+	v.Add("friend", "Jess")
+	v.Add("friend", `{"value": "hello"}`)
+	v.Add("friend", "Zoe")
+	aa := v.Encode()
+
+	aaa, _ := url.Parse(aa)
+
+	fmt.Println(aa)
+	fmt.Println(aaa.RawQuery)
+	fmt.Println(data.Prefectures[0].Name)
+	fmt.Println(data.Prefectures[1].Name)
 }
