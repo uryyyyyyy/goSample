74131c54b61eb1ac76adb9bd1bbb44e282468f7a
uryyyyyyy <koki305@gmail.com>
2017-03-09 02:13:09 +0900

    pubsub

--- main.go
+++ main.go
@@ -3,15 +3,36 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"github.com/franela/goreq"
+	"io"
+	"strconv"
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/log"
 )
 
 func init() {
-	http.HandleFunc("/hello", handler)
+	http.HandleFunc("/", handler)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	ctx := appengine.NewContext(r)
+	length, err := strconv.Atoi(r.Header.Get("Content-Length"))
+	if err != nil {
+		log.Debugf(ctx, "Content-Length error")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	body := make([]byte, length)
+	log.Debugf(ctx, strconv.Itoa(length))
+	length, err2 := r.Body.Read(body)
+	if err2 != nil && err2 != io.EOF {
+		log.Debugf(ctx, "r.Body.Read error: " + err2.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	log.Debugf(ctx, "request URL: " + r.URL.Path)
+	json := string(body)
+	log.Debugf(ctx, "r.Body.Read " + json)
 	fmt.Fprint(w, "Hello, world!")
-	res, _ := goreq.Request{ Uri: "http://www.google.com" }.Do()
-	fmt.Fprint(w, "Hello, world!" + res.Status)
 }
